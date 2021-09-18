3dcd60aaad877ab966a47c697aa301a9111fb1b2
Matheus <matheus_c_o@yahoo.com.br>
2021-09-17 23:57:48 -0300

    glóriaaaaaa

---
+++ api/src/index.js
@@ -0,0 +1,117 @@
+import db from './db.js';
+import express from 'express'
+import cors from 'cors'
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/matricula', async (req, resp) => {
+    try {
+        let alunos = await db.tb_matricula.findAll({ order: [['id_matricula', 'desc']] });
+        resp.send(alunos);
+    } catch (e) {
+        resp.send({ erro: 'Ocorreu um erro!'})
+    }
+})
+
+app.post('/matricula', async (req, resp) => {
+    try {
+
+        
+        let { nome, chamada, curso, turma } = req.body;
+
+        let p = await db.tb_matricula.findOne({where: { nm_turma: turma } } );
+        let q = await db.tb_matricula.findOne({where: { nr_chamada: chamada} } );
+
+        let matriculaa = {
+            nm_aluno: nome,
+            nr_chamada: chamada,
+            nm_curso: curso,
+            nm_turma: turma
+             
+        }
+
+        if(!isNaN(chamada) == false) {
+            return resp.send({ erro: 'No campo Chamada coloque apenas numeros!' })
+        } 
+        
+        if(nome == '' || chamada == '' || curso == '' || turma == '') {
+            return resp.send({ erro: 'Você esqueceu de preencher os campos' })
+        }
+        if(nome.length <= 4 || curso.length <= 4 || turma.length <= 4) {
+            return resp.send({ erro: 'Coloque mais que 4 caracteres nos campos abaixo' })
+        }
+        
+        
+        if(chamada <= 0)
+            return resp.send({ erro: 'Coloque um numero maior que 0' })
+
+
+        
+        if(p != null && q != null){
+            return resp.send({ erro: 'Aluno ja cadastrado' })
+        }
+        
+        let r = await db.tb_matricula.create(matriculaa);
+        resp.send(r);
+        
+    } catch (e) {
+        resp.send({ erro: e.toString() })
+    }
+});
+
+app.delete('/matricula/:id', async (req, resp) => {
+    try {
+        let r = await db.tb_matricula.destroy({ where: { id_matricula: req.params.id} })
+        resp.sendStatus(200);
+        
+    } catch (e) {
+        resp.send({ erro: e.toString() })
+        
+    }
+})
+
+app.put('/matricula/:id', async (req, resp) => {
+    try {
+        let id = req.params.id;
+        let { nome, chamada, curso, turma } = req.body;
+
+        if(!isNaN(chamada) == false) {
+            return resp.send({ erro: 'No campo Chamada coloque apenas numeros!' })
+        } 
+        
+        if(nome == '' || chamada == '' || curso == '' || turma == '') {
+            return resp.send({ erro: 'Você esqueceu de preencher os campos' })
+        }
+        if(nome.length <= 4 || curso.length <= 4 || turma.length <= 4) {
+            return resp.send({ erro: 'Coloque mais que 4 caracteres nos campos abaixo' })
+        }
+        
+        
+        if(chamada <= 0)
+            return resp.send({ erro: 'Coloque um numero maior que 0' })
+
+
+        let r = await db.tb_matricula.update (
+            {
+                nm_aluno: nome,
+                nr_chamada: chamada,
+                nm_curso: curso,
+                nm_turma: turma
+            },
+            {
+                where: { id_matricula: id }
+            }
+        );
+        resp.sendStatus(200)
+
+    }catch (e) {
+        resp.send({ erro: e.toString() });
+    }
+})
+
+
+
+app.listen(process.env.PORT, x => console.log(`>> Server up at port ${process.env.PORT}`))
---
+++ react/site/src/components/menu/conteudo.js
@@ -0,0 +1,193 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css'; 
+
+import LoadingBar from 'react-top-loading-bar'
+
+import { useState, useEffect, useRef } from 'react'
+import { ContainerConteudo, Barra } from './conteudostyle'
+
+import Api from '../../service/api'
+const api = new Api()
+
+
+export default function Conteudo() {
+    const [alunos, setAlunos] = useState([])
+    const [nome, setNome] = useState('')
+    const [chamada, setChamada] = useState('')
+    const [turma, setTurma] = useState('')
+    const [curso, setCurso] = useState('')
+    const [idAlterando, setIdAlterando] = useState(0)
+
+    const loading = useRef(null);
+
+    async function listar () {
+        loading.current.continuousStart();
+
+        let r = await api.listar()
+        setAlunos(r)
+
+        loading.current.complete();
+    }
+
+    async function inserir() {
+        if (idAlterando == 0) {
+            let r = await api.inserir( nome, chamada, curso, turma)
+
+            if (r.erro)
+                toast.error(`❌ ${r.erro}`)
+            else 
+                toast.dark('✔️ Aluno Cadastrado com sucesso');
+
+        } else {
+            let r = await api.alterar(idAlterando, nome, chamada, curso, turma)
+            
+            if (r.erro)
+                toast.error(`❌ ${r.erro}`)
+            else 
+                toast.dark('✔️ Aluno alterado com sucesso');
+
+        }
+        
+        limparDados()
+        listar()
+    }
+    function limparDados() {
+        setNome('')
+        setChamada('')
+        setCurso('')
+        setTurma('')
+        setIdAlterando(0)
+    }
+
+    async function remover(id) {
+        confirmAlert({
+            title: 'Remover aluno',
+            message: `Tem certeza que deseja remover o aluno ${id} ?`,
+            buttons: [
+              {
+                label: 'Sim', 
+                onClick: async () => {
+                    let r = await api.remover(id);
+                    if (r.erro)
+                        toast.error(`${r.erro}`);
+                    else {
+                        toast.dark('✔️ Aluno removido!')
+                        listar();
+                    }
+                }
+              },
+              {
+                label: 'Não'
+              }
+            ]
+        });
+    }
+
+    async function editar(item) {
+        setNome(item.nm_aluno)
+        setChamada(item.nr_chamada)
+        setCurso(item.nm_curso)
+        setTurma(item.id_matricula)
+        setIdAlterando(item.id_matricula)
+
+    }
+    useEffect(() => {
+        listar()
+    }, [])
+    
+    return (
+        <ContainerConteudo>
+            <ToastContainer />
+            <LoadingBar color="red" ref={loading} />
+            <div className="container1">
+                <div className="sloga">
+                    <img className="logo" src="/assents/images/chevron-down.svg" alt="" />
+                    <div className="titulo"><span>Dev</span> School</div>
+                </div>
+                <div className="vazio">
+
+                </div>
+                <div className="geren">
+                    <div className="opcao1"> Gerenciamento <img className="setaprabaixo" src="/assents/images/chevron-down.svg" alt="" /> </div>
+                </div>
+                <div className="alun">
+                    <div className="barra1roxo"></div>
+                    <div className="opcao2">Alunos</div>
+                </div>
+            </div>
+            <div className="container2">
+                <div className="cabecalho">
+                    <div className="usuario-imagem">  
+                        <div className="usuario">
+                            <img src="/assents/images/Cirilops.jpg"/> 
+                            <div className="bullet">3</div>
+                        </div>
+                        <div className="mensagem-usu"> Olá, Cirilão </div>   
+                    </div>
+                    <div className="contalunos">
+                        <img className="recarregar" src="/assents/images/refresh.svg" alt="" />
+                    </div>
+                    <div className="contalunos">
+                        <img className="logout" src="/assents/images/log-out.svg" alt="" />
+                    </div>
+                </div>
+                <div className="cadrastro">
+                    <div className="cab"> 
+                    <Barra/>
+                    <div className="titulo-alun"> {idAlterando == 0 ? "Novo Aluno" : "Alterando Aluno " + idAlterando}</div>
+                    </div>
+                    <div className="cxinputs">
+                        <div className="dados">
+                            <div className="nome"> Nome: <input type="text" value={nome} onChange={e => setNome(e.target.value)}  /> </div>
+                            <div className="curso"> Curso: <input type="text" value={curso} onChange={e => setCurso(e.target.value)}/> </div>
+                            <div className="chamada"> Chamada: <input type="text" value={chamada} onChange={e => setChamada(e.target.value)}/> </div>
+                            <div className="turma"> Turma: <input type="text" value={turma} onChange={e => setTurma(e.target.value)}/> </div>
+                        </div>
+                        <div className="botao"> <button onClick={inserir}> {idAlterando == 0 ? "Cadastrar" : "Alterar"} </button> </div>
+                    </div>
+                </div> 
+                <div className="listaalunos">
+                    <div className="cab">
+                        <Barra/>
+                        <div className="titulo-matri"> Alunos Matriculados</div>
+                    </div>
+
+                    <table className="lista-tb">
+                    <thead>
+                        <tr className="cabecalhoTb">
+                            <th className="idTb">ID</th>
+                            <th className="alunoTb">Nome</th>
+                            <th className="numeroTb">Chamada</th>
+                            <th className="turmaTb">Turma</th>
+                            <th className="cursoTb">Curso</th>
+                            <th className="espaço"></th>
+                            <th className="espaço"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {alunos.map((item, i) => 
+                            <tr className={i % 2 === 0 ? "linha-alternada" : ""}>
+                                <td className="idTb1">{item.id_matricula}</td>
+                                
+                                <td title={item.nm_aluno}>
+                                    {item.nm_aluno != null && item.nm_aluno.length >= 25
+                                        ? item.nm_aluno.substr(0, 25) + '...'  : item.nm_aluno}       
+                                </td>
+                                <td>{item.nr_chamada}</td>
+                                <td>{item.nm_turma}</td>
+                                <td>{item.nm_curso}</td>
+                                <td className="botao-visivel"> <button onClick={() => editar(item)}> <img src="/assents/images/edit.svg" alt="" /> </button> </td>
+                                <td className="botao-visivel"> <button onClick={() => remover(item.id_matricula)}  > <img src="/assents/images/trash.svg" alt="" /> </button> </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+                </div>
+            </div>
+        </ContainerConteudo>
+        
+        
+    )
+}
